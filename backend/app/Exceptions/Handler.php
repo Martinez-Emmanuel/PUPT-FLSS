ed50015149b31114d372f7b22a912153853da9e9
Naoe-Adrian <Adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-08-01 15:23:54 +0800

    add readable error in handler

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
@@ -7,24 +6,33 @@ use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
+    protected $dontReport = [
+        //
+    ];
+
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
-    public function register(): void
+    public function register()
     {
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($request->expectsJson()) {
+            $status = method_exists($exception, 'getStatusCode') ? $exception->getStatusCode() : 500;
+
+            return response()->json([
+                'message' => $exception->getMessage()
+            ], $status);
+        }
+
+        return parent::render($request, $exception);
     }
 }
