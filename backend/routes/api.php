3cbcd84605ec2f6e8e9622d6a5f332fe6449534f
Naoe-Adrian <Adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-08-01 15:24:53 +0800

    Update API routes and add authentication middleware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,22 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FacultyController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::post('login', [AuthController::class, 'login'])->name('login');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/some-protected-route', function (Request $request) {
+        return response()->json(['message' => 'You are authenticated']);
+    });
 });
-
-Route::get('faculties', [FacultyController::class, 'getFaculty']);
