597cb479d410f100396ad6ed6e6f5f3d5d84ea81
Martinez-Emmanuel <emmanuellmartinez013@gmail.com>
2024-08-01 19:47:09 +0800

    add sub-routes to root routing config

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,12 +7,33 @@ export const routes: Routes = [
       import('./auth/login/login.component').then((m) => m.LoginComponent),
   },
   {
+    path: 'faculty',
+    loadChildren: () =>
+      import('./core/components/faculty/faculty.routes').then(
+        (m) => m.FACULTY_ROUTES
+      ),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./core/components/admin/admin.routes').then(
+        (m) => m.ADMIN_ROUTES
+      ),
+  },
+  {
+    path: 'superadmin',
+    loadChildren: () =>
+      import('./core/components/superadmin/superadmin.routes').then(
+        (m) => m.SUPERADMIN_ROUTES
+      ),
+  },
+  {
     path: '',
     redirectTo: '/login',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: '**',
-    redirectTo: '/login'
-  }
+    redirectTo: '/login',
+  },
 ];
