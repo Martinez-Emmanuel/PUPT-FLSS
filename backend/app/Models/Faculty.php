d48efb2224e5a4b65eee9ea6cc69731b9b63a8b2
Naoe-Adrian <Adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-08-01 15:24:30 +0800

    update faculty php

--- backend/app/Models/Faculty.php
+++ backend/app/Models/Faculty.php
@@ -2,15 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
-class Faculty extends Model
+class Faculty extends Model implements AuthenticatableContract
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Authenticatable;
+
     protected $table = 'faculties';
     protected $primaryKey = 'faculty_id'; // Specify the primary key
 
     public $timestamps = false;
-    protected $fillable = ['faculty_name', 'faculty_code', 'faculty_password', 'faculty_email', 'faculty_type'];
+
+    protected $fillable = [
+        'faculty_name', 
+        'faculty_code', 
+        'faculty_password', 
+        'faculty_email', 
+        'faculty_type'
+    ];
 }
+
