a26151f11286ff983bc952ed28cc35f03a3dca43
Naoe-Adrian <Adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-08-01 15:24:42 +0800

    update sanctum code

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -3,23 +3,14 @@
 use Laravel\Sanctum\Sanctum;
 
 return [
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1')),
+    'guard' => ['web'],
+    'expiration' => null,
+    'middleware' => [
+        'verify_csrf_token' => \App\Http\Middleware\VerifyCsrfToken::class,
+        'encrypt_cookies' => \App\Http\Middleware\EncryptCookies::class,
+    ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Stateful Domains
-    |--------------------------------------------------------------------------
-    |
-    | Requests from the following domains / hosts will receive stateful API
-    | authentication cookies. Typically, these should include your local
-    | and production domains which access your API via a frontend SPA.
-    |
-    */
-
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort()
-    ))),
 
     /*
     |--------------------------------------------------------------------------
