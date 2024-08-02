a786048cdefd016db00c598e27ae19aa1fb6a25f
Martinez-Emmanuel <emmanuellmartinez013@gmail.com>
2024-08-02 21:12:49 +0800

    import additional material modules

--- frontend/src/app/core/imports/material.component.ts
+++ frontend/src/app/core/imports/material.component.ts
@@ -1,8 +1,16 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
 
-export const MaterialComponent = [
-  MatIconModule,
+export const MaterialComponents = [
+  MatToolbarModule,
   MatButtonModule,
+  MatSidenavModule,
+  MatListModule,
+  MatIconModule,
+  MatTabsModule
   // ... other material modules
 ];
