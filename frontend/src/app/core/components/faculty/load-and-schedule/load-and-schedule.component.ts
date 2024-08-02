2032bf1067caa6a7a3aa2beecbe7db4fb94da73c
Martinez-Emmanuel <emmanuellmartinez013@gmail.com>
2024-08-02 21:15:41 +0800

    add initial load-and-sched component

---
+++ frontend/src/app/core/components/faculty/load-and-schedule/load-and-schedule.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-load-and-schedule',
+  standalone: true,
+  imports: [],
+  templateUrl: './load-and-schedule.component.html',
+  styleUrl: './load-and-schedule.component.scss'
+})
+export class LoadAndScheduleComponent {
+
+}
