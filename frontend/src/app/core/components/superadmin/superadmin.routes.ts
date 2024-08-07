a8be0e19112e21b40e6b7a714af9e8fe879b1c66
Martinez-Emmanuel <emmanuellmartinez013@gmail.com>
2024-08-07 12:16:02 +0800

    add initial superadmin routing

--- frontend/src/app/core/components/superadmin/superadmin.routes.ts
+++ frontend/src/app/core/components/superadmin/superadmin.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const SUPERADMIN_ROUTES: Routes = [];
+export const SUPERADMIN_ROUTES: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./main/main.component').then((m) => m.MainComponent),
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./dashboard/dashboard.component').then(
+            (m) => m.DashboardComponent
+          ),
+      },
+      {
+        path: '**',
+        redirectTo: 'dashboard',
+      },
+    ],
+  },
+];
