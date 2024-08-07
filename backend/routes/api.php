b58db1233aeb2b765982afd03b1a867101087970
Naoe-Adrian <adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-08-07 17:49:49 +0800

    Add API endpoint for sending emails to faculties

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,7 +5,11 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\PreferenceController;
+use App\Http\Controllers\FacultyController;
 Route::post('login', [AuthController::class, 'login'])->name('login');
+
+
+
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
@@ -16,4 +20,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/submittedPreferences', [PreferenceController::class, 'getAllSubmittedPreferences']);
     Route::get('/courses', [CourseController::class, 'index']);
     Route::post('/addCourse', [CourseController::class, 'addCourse']);
+    Route::post('faculties/send-emails', [FacultyController::class, 'sendEmails']);
 });
