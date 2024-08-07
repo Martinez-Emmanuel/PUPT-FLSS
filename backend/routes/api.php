e9eb49e64cebb12c77fc89e25c0b65be6f145a63
Naoe-Adrian <adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-08-07 09:31:00 +0800

    Add API routes for adding courses and submitting preferences

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,12 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
-
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\PreferenceController;
 Route::post('login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/addCourse', [CourseController::class, 'addCourse']);
     Route::get('/some-protected-route', function (Request $request) {
         return response()->json(['message' => 'You are authenticated']);
     });
+    Route::post('/submitPreference', [PreferenceController::class, 'submitPreference']);
 });
