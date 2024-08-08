0bee4eddfe64f0ef39be066416c2f73847f5d950
Martinez-Emmanuel <emmanuellmartinez013@gmail.com>
2024-08-08 14:30:53 +0800

    add superadmin main component

--- frontend/src/app/core/components/superadmin/main/main.component.ts
+++ frontend/src/app/core/components/superadmin/main/main.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 import { RouterModule } from '@angular/router';
+
+import { ThemeService } from '../../../services/theme/theme.service';
+import { MaterialComponents } from '../../../imports/material.component';
+import { MatSymbolDirective } from '../../../imports/mat-symbol.directive';
 
 @Component({
   selector: 'app-main',
-  standalone: true,
-  imports: [RouterModule],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    AsyncPipe,
+    RouterModule,
+    MaterialComponents,
+    MatSymbolDirective,
+  ],
 })
 export class MainComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+  public isDropdownOpen = false;
 
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
+
+  constructor(public themeService: ThemeService) {}
+
+  toggleTheme() {
+    this.themeService.toggleTheme();
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
 }
