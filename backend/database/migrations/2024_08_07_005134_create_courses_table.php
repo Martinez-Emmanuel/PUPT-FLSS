b090eb6cf0e5cde85ddaafffa25aacc241b3d103
Naoe-Adrian <adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-08-21 21:13:35 +0800

    Update courses table migration

--- backend/database/migrations/2024_08_07_005134_create_courses_table.php
+++ backend/database/migrations/2024_08_07_005134_create_courses_table.php
@@ -9,13 +9,20 @@ class CreateCoursesTable extends Migration
     public function up()
     {
         Schema::create('courses', function (Blueprint $table) {
-            $table->bigIncrements('course_id');
-            $table->string('subject_code');
-            $table->string('subject_title');
+            $table->increments('course_id');
+            $table->unsignedInteger('semester_id')->nullable(); 
+            $table->string('course_code', 10);
+            $table->string('course_title', 100);
             $table->integer('lec_hours');
             $table->integer('lab_hours');
-            $table->integer('total_units');
+            $table->integer('units');
+            $table->integer('tuition_hours');
             $table->timestamps();
+
+            $table->foreign('semester_id')
+                  ->references('semester_id')
+                  ->on('semesters')
+                  ->onDelete('set null');
         });
     }
 
