8a9959f31d3a8bde18cee63d9c146085392130d6
Martinez-Emmanuel <emmanuellmartinez013@gmail.com>
2024-08-22 18:37:10 +0800

    add float animation

--- frontend/src/app/core/animations/animations.ts
+++ frontend/src/app/core/animations/animations.ts
@@ -46,3 +46,13 @@ export const cardEntranceAnimation = trigger('cardEntranceAnimation', [
     ),
   ]),
 ]);
+
+export const pageFloatUpAnimation = trigger('pageFloatUpAnimation', [
+  transition(':enter', [
+    style({ opacity: 0, transform: 'translateY(20px)' }),
+    animate('400ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+  ]),
+  transition(':leave', [
+    animate('300ms ease-in', style({ opacity: 0, transform: 'translateY(20px)' }))
+  ])
+]); 
