d0f04258566f2ad446690e00ac9981dc6b012089
Naoe-Adrian <adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-08-26 12:30:03 +0800

    Update ProgramsTableSeeder to remove unnecessary program entries

--- backend/database/seeders/ProgramsTableSeeder.php
+++ backend/database/seeders/ProgramsTableSeeder.php
@@ -36,28 +36,6 @@ class ProgramsTableSeeder extends Seeder
                 'created_at' => $currentTimestamp,
                 'updated_at' => $currentTimestamp,
             ],
-            [
-                'program_id' => 3,
-                'curriculum_id' => 2,
-                'program_code' => 'BSIT',
-                'program_title' => 'Bachelor of Science in Information Technology',
-                'program_info' => 'Updated curriculum with new courses.',
-                'status' => 'active',
-                'number_of_years' => 4,
-                'created_at' => $currentTimestamp,
-                'updated_at' => $currentTimestamp,
-            ],
-            [
-                'program_id' => 4,
-                'curriculum_id' => 2,
-                'program_code' => 'BSA',
-                'program_title' => 'Bachelor of Science in Accountancy',
-                'program_info' => 'Updated curriculum with emphasis on analytics.',
-                'status' => 'active',
-                'number_of_years' => 4,
-                'created_at' => $currentTimestamp,
-                'updated_at' => $currentTimestamp,
-            ],
         ]);
 
         
