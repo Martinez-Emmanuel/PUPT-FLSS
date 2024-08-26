a55a795e657f6f92d62821b6cd250a76d306e3fa
Naoe-Adrian <adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-08-26 20:12:35 +0800

    Refactor Program model to use 'curricula_program_id' instead of 'program_id'

--- backend/app/Models/Program.php
+++ backend/app/Models/Program.php
@@ -7,12 +7,42 @@ use Illuminate\Database\Eloquent\Model;
 
 class Program extends Model
 {
+    // use HasFactory;
+
+    // protected $primaryKey = 'program_id';
+
+    // protected $fillable = [
+    //     'curriculum_id',
+    //     'program_code',
+    //     'program_title',
+    //     'program_info',
+    //     'status',
+    //     'number_of_years',
+    // ];
+
+    // public function curriculum()
+    // {
+    //     return $this->belongsTo(Curriculum::class, 'curriculum_id', 'curriculum_id');
+    // }
+
+    // public function yearLevels()
+    // {
+    //     return $this->hasMany(YearLevel::class, 'program_id', 'program_id');
+    // }
+
+    // public function courseAssignments()
+    // {
+    //     return $this->hasMany(CourseAssignment::class, 'program_id', 'program_id');
+    // }
+    // public function curricula()
+    // {
+    //     return $this->belongsToMany(Curriculum::class, 'curricula_program', 'program_id', 'curriculum_id');
+    // }
     use HasFactory;
 
     protected $primaryKey = 'program_id';
 
     protected $fillable = [
-        'curriculum_id',
         'program_code',
         'program_title',
         'program_info',
@@ -20,22 +50,13 @@ class Program extends Model
         'number_of_years',
     ];
 
-    public function curriculum()
+    public function curricula()
     {
-        return $this->belongsTo(Curriculum::class, 'curriculum_id', 'curriculum_id');
+        return $this->belongsToMany(Curriculum::class, 'curricula_program', 'program_id', 'curriculum_id');
     }
 
     public function yearLevels()
     {
         return $this->hasMany(YearLevel::class, 'program_id', 'program_id');
-    }
-
-    public function courseAssignments()
-    {
-        return $this->hasMany(CourseAssignment::class, 'program_id', 'program_id');
-    }
-    public function curricula()
-    {
-        return $this->belongsToMany(Curriculum::class, 'curricula_program', 'program_id', 'curriculum_id');
     }
 }
