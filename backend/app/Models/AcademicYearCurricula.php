0dc0f05b7217ee7fd69945bf545fb4e2912880d5
Martinez-Emmanuel <emmanuellmartinez013@gmail.com>
2024-09-10 20:57:25 +0800

    add models for the new scheduling-related tables

---
+++ backend/app/Models/AcademicYearCurricula.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AcademicYearCurricula extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'academic_year_curricula_id';
+
+    protected $fillable = [
+        'academic_year_id',
+        'curriculum_id',
+    ];
+
+    public function academicYear()
+    {
+        return $this->belongsTo(AcademicYear::class, 'academic_year_id', 'academic_year_id');
+    }
+
+    public function curriculum()
+    {
+        return $this->belongsTo(Curriculum::class, 'curriculum_id', 'curriculum_id');
+    }
+}
---
+++ backend/app/Models/ProgramYearLevelCurricula.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProgramYearLevelCurricula extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'program_year_level_curricula_id';
+
+    protected $fillable = [
+        'academic_year_id',
+        'program_id',
+        'year_level',
+        'curriculum_id',
+    ];
+
+    public function academicYear()
+    {
+        return $this->belongsTo(AcademicYear::class, 'academic_year_id', 'academic_year_id');
+    }
+
+    public function program()
+    {
+        return $this->belongsTo(Program::class, 'program_id', 'program_id');
+    }
+
+    public function curriculum()
+    {
+        return $this->belongsTo(Curriculum::class, 'curriculum_id', 'curriculum_id');
+    }
+}
