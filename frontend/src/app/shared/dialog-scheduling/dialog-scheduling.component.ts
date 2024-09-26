239115e05402c7cde8686b93f5f567654a820a87
Martinez-Emmanuel <emmanuellmartinez013@gmail.com>
2024-09-26 12:43:27 +0800

    add autocomplete fields

--- frontend/src/app/shared/dialog-scheduling/dialog-scheduling.component.ts
+++ frontend/src/app/shared/dialog-scheduling/dialog-scheduling.component.ts
@@ -1,14 +1,17 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { MatDialogModule } from '@angular/material/dialog';
+import { Observable, of } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 import { MatSymbolDirective } from '../../core/imports/mat-symbol.directive';
 
@@ -24,19 +27,24 @@ import { MatSymbolDirective } from '../../core/imports/mat-symbol.directive';
     MatButtonModule,
     ReactiveFormsModule,
     MatSelectModule,
+    MatAutocompleteModule,
     MatSymbolDirective,
   ],
   templateUrl: './dialog-scheduling.component.html',
   styleUrls: ['./dialog-scheduling.component.scss'],
 })
-export class DialogSchedulingComponent {
+export class DialogSchedulingComponent implements OnInit {
   scheduleForm: FormGroup;
   dayOptions: string[];
   timeOptions: string[];
   endTimeOptions: string[];
   selectedProgramInfo: string;
   selectedCourseInfo: string;
-  suggestedFaculty: { name: string; time: string }[];
+  suggestedFaculty: { name: string; day: string; time: string }[];
+  hasConflicts: boolean = false;
+
+  filteredProfessors: Observable<string[]>;
+  filteredRooms: Observable<string[]>;
 
   constructor(
     private fb: FormBuilder,
@@ -57,6 +65,30 @@ export class DialogSchedulingComponent {
       professor: ['', Validators.required],
       room: ['', Validators.required],
     });
+
+    this.filteredProfessors = of([]);
+    this.filteredRooms = of([]);
+  }
+
+  ngOnInit() {
+    this.filteredProfessors = this.scheduleForm
+      .get('professor')!
+      .valueChanges.pipe(
+        startWith(''),
+        map((value) => this._filter(value || '', this.data.professorOptions))
+      );
+
+    this.filteredRooms = this.scheduleForm.get('room')!.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value || '', this.data.roomOptions))
+    );
+  }
+
+  private _filter(value: string, options: string[]): string[] {
+    const filterValue = value.toLowerCase();
+    return options.filter((option) =>
+      option.toLowerCase().includes(filterValue)
+    );
   }
 
   onCancel() {
@@ -65,7 +97,6 @@ export class DialogSchedulingComponent {
 
   onAssign() {
     if (this.scheduleForm.valid) {
-      console.log(this.scheduleForm.value);
       this.dialogRef.close(this.scheduleForm.value);
     } else {
       this.scheduleForm.markAllAsTouched();
