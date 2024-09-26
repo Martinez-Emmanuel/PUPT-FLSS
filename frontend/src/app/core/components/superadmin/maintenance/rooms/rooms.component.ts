adbb0e3296f6589ee49e2bdb049d82aaf7af6d2d
Martinez-Emmanuel <emmanuellmartinez013@gmail.com>
2024-09-26 09:56:09 +0800

    minor ui improvements

--- frontend/src/app/core/components/superadmin/maintenance/rooms/rooms.component.ts
+++ frontend/src/app/core/components/superadmin/maintenance/rooms/rooms.component.ts
@@ -247,7 +247,7 @@ export class RoomsComponent implements OnInit {
           label: 'Status',
           formControlName: 'status',
           type: 'select',
-          options: ['Available', 'Unavailable'],
+          options: ['available', 'unavailable'],
           required: true,
         },
       ],
