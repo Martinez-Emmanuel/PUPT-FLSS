5c81259a700ba00d9f844608c8fbc02735eaec98
Martinez-Emmanuel <emmanuellmartinez013@gmail.com>
2024-10-06 19:24:36 +0800

    prepare frontend for programs reports

--- frontend/src/app/core/components/admin/reports/report-programs/report-programs.component.ts
+++ frontend/src/app/core/components/admin/reports/report-programs/report-programs.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatIconModule } from '@angular/material/icon';
-import { TableHeaderComponent, InputField } from '../../../../../shared/table-header/table-header.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialog } from '@angular/material/dialog';
+
+import { InputField, TableHeaderComponent } from '../../../../../shared/table-header/table-header.component';
+import { TableDialogComponent, DialogConfig, DialogFieldConfig } from '../../../../../shared/table-dialog/table-dialog.component';
 
 import { fadeAnimation } from '../../../../animations/animations';
 
@@ -14,39 +19,178 @@ import { fadeAnimation } from '../../../../animations/animations';
   standalone: true,
   imports: [
     CommonModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
+    MatTableModule,
+    MatPaginatorModule,
     MatIconModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    MatTooltipModule,
     TableHeaderComponent,
+    TableDialogComponent,
   ],
   templateUrl: './report-programs.component.html',
-  styleUrls: ['./report-programs.component.scss'],
+  styleUrl: './report-programs.component.scss',
   animations: [fadeAnimation],
 })
 export class ReportProgramsComponent {
   inputFields: InputField[] = [
     {
-      type: 'select',
-      label: 'Program',
-      key: 'program',
-      options: ['Program A', 'Program B', 'Program C'],
+      type: 'text',
+      label: 'Search Programs',
+      key: 'search',
+    },
+  ];
+  displayedColumns: string[] = [
+    'index',
+    'programCode',
+    'programName',
+    'yearLevel',
+    'section',
+    'action',
+  ];
+  dataSource = [
+    {
+      programName: 'Bachelor of Science in Information Technology',
+      programCode: 'BSIT-TG',
+      yearLevel: '1st Year',
+      section: '1',
     },
     {
-      type: 'select',
-      label: 'Year Level',
-      key: 'yearLevel',
-      options: ['1', '2', '3', '4'],
+      programName: 'Bachelor of Science in Mechanical Engineering',
+      programCode: 'BSME-TG',
+      yearLevel: '1st Year',
+      section: '1',
     },
     {
-      type: 'select',
-      label: 'Section',
-      key: 'section',
-      options: ['Section A', 'Section B', 'Section C'],
+      programName: 'Bachelor of Science in Computer Science',
+      programCode: 'BSCS-TG',
+      yearLevel: '2nd Year',
+      section: '1',
+    },
+    {
+      programName: 'Bachelor of Science in Electrical Engineering',
+      programCode: 'BSEE-TG',
+      yearLevel: '2nd Year',
+      section: '1',
+    },
+    {
+      programName: 'Bachelor of Arts in Business Administration',
+      programCode: 'BBA-TG',
+      yearLevel: '1st Year',
+      section: '1',
+    },
+    {
+      programName: 'Bachelor of Science in Civil Engineering',
+      programCode: 'BSCE-TG',
+      yearLevel: '3rd Year',
+      section: '1',
+    },
+    {
+      programName: 'Bachelor of Science in Nursing',
+      programCode: 'BSN-TG',
+      yearLevel: '1st Year',
+      section: '1',
+    },
+    {
+      programName: 'Bachelor of Science in Psychology',
+      programCode: 'BSP-TG',
+      yearLevel: '2nd Year',
+      section: '1',
+    },
+    {
+      programName: 'Bachelor of Science in Hospitality Management',
+      programCode: 'BSHM-TG',
+      yearLevel: '1st Year',
+      section: '1',
+    },
+    {
+      programName: 'Bachelor of Fine Arts',
+      programCode: 'BFA-TG',
+      yearLevel: '4th Year',
+      section: '1',
+    },
+    {
+      programName: 'Bachelor of Science in Marketing',
+      programCode: 'BSM-TG',
+      yearLevel: '2nd Year',
+      section: '1',
+    },
+    {
+      programName: 'Bachelor of Science in Accounting',
+      programCode: 'BSAc-TG',
+      yearLevel: '4th Year',
+      section: '1',
     },
   ];
 
-  activeYear = '2024-2025';
-  activeSemester = '1st Semester';
+  filteredData = this.dataSource;
+  paginator: any;
+
+  constructor(private dialog: MatDialog) {}
+
+  onOpenDialog(element: any, field: string) {
+    let dialogFields: DialogFieldConfig[] = [];
+    let title = '';
+
+    if (field === 'yearLevel') {
+      dialogFields = [
+        {
+          label: 'Year Level',
+          formControlName: 'yearLevel',
+          type: 'select',
+          options: ['All', '1', '2', '3', '4'],
+          required: true,
+        },
+      ];
+      title = 'Select Year Level';
+    } else if (field === 'section') {
+      dialogFields = [
+        {
+          label: 'Section',
+          formControlName: 'section',
+          type: 'select',
+          options: ['All', '1'],
+          required: true,
+        },
+      ];
+      title = 'Select Section';
+    }
+
+    const dialogConfig: DialogConfig = {
+      title,
+      fields: dialogFields,
+      isEdit: true,
+      initialValue: { [field]: element[field] },
+    };
+
+    const dialogRef = this.dialog.open(TableDialogComponent, {
+      data: dialogConfig,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        element[field] = result[field];
+      }
+    });
+  }
+
+  onExportAll() {
+    console.log('Export All clicked');
+  }
+
+  onInputChange(event: any) {
+    console.log('Input Change:', event);
+  }
+
+  onView(element: any) {
+    console.log('View clicked:', element);
+  }
+
+  onExportSingle(element: any) {
+    console.log('Export Single clicked:', element);
+  }
+
+  updateDisplayedData() {
+    console.log('Page changed');
+  }
 }
