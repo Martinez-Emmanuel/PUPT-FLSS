64238fdff10f085ace3384a08b72f8b65234c5a3
Martinez-Emmanuel <emmanuellmartinez013@gmail.com>
2024-10-09 17:48:08 +0800

    add initial dialog-view-scheduling component

---
+++ frontend/src/app/shared/dialog-view-schedule/dialog-view-schedule.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-view-schedule',
+  standalone: true,
+  imports: [],
+  templateUrl: './dialog-view-schedule.component.html',
+  styleUrl: './dialog-view-schedule.component.scss'
+})
+export class DialogViewScheduleComponent {
+
+}
