959418c7304ac528ff3678fe2541363f8cc10a8d
Naoe-Adrian <Adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-10-30 10:27:18 +0800

    add encryption key to production environment configuration

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://api.pupt-flss.com/api',
+  encryptionKey: '2fc3e75b53bacf73038ff1ab41f0d8fa399edcaba1faf3c05ad0b419d6788175'  
 };
