062cb9db4daf2e17b094a1f8fafdaa2eb0233433
Naoe-Adrian <Adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-10-30 10:26:49 +0800

    toggle production flag in environment files and add encryption key for development

--- frontend/src/environments/environment.dev.ts
+++ frontend/src/environments/environment.dev.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'http://127.0.0.1:8000/api',
+  encryptionKey: '2fc3e75b53bacf73038ff1ab41f0d8fa399edcaba1faf3c05ad0b419d6788175'  
 };
