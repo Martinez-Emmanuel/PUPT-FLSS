98d275d4a6ab45e22d8ba98fcda3264b1ac3a66e
Martinez-Emmanuel <emmanuellmartinez013@gmail.com>
2024-11-06 21:58:13 +0800

    add global animation configuration to matdialog

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 import { MAT_RIPPLE_GLOBAL_OPTIONS, RippleGlobalOptions } from '@angular/material/core';
+import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
 
 import { routes } from './app.routes';
 import { AuthGuard } from './core/guards/auth.guard';
@@ -10,8 +12,8 @@ import { AuthGuard } from './core/guards/auth.guard';
 const globalRippleConfig: RippleGlobalOptions = {
   animation: {
     enterDuration: 500,
-    exitDuration: 150
-  }
+    exitDuration: 150,
+  },
 };
 
 export const appConfig: ApplicationConfig = {
@@ -27,5 +29,12 @@ export const appConfig: ApplicationConfig = {
     AuthGuard,
 
     { provide: MAT_RIPPLE_GLOBAL_OPTIONS, useValue: globalRippleConfig },
+
+    {
+      provide: MAT_DIALOG_DEFAULT_OPTIONS,
+      useValue: {
+        exitAnimationDuration: '250ms',
+      },
+    },
   ],
 };
