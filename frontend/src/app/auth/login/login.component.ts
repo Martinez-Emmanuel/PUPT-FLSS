fd8593036e5f656f0c8a87bfe7c1e042c6977a9b
Martinez-Emmanuel <emmanuellmartinez013@gmail.com>
2025-01-23 13:21:51 +0800

    optimize image slideshow in login component

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit, OnDestroy, Renderer2, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { Subject, Observable, takeUntil, forkJoin, timer } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
 
-import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRippleModule } from '@angular/material/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialog } from '@angular/material/dialog';
 import { MatSymbolDirective } from '../../core/imports/mat-symbol.directive';
 
-import { SlideshowComponent } from '../../shared/slideshow/slideshow.component';
 import { DialogFacultyLoginComponent } from '../../shared/dialog-faculty-login/dialog-faculty-login.component';
 import { DialogAdminLoginComponent } from '../../shared/dialog-admin-login/dialog-admin-login.component';
 import { DialogRedirectComponent } from '../../shared/dialog-redirect/dialog-redirect.component';
+import { SlideshowComponent } from '../../shared/slideshow/slideshow.component';
 
 import { ThemeService } from '../../core/services/theme/theme.service';
 import { AuthService } from '../../core/services/auth/auth.service';
@@ -40,7 +40,7 @@ export class LoginComponent implements OnInit, OnDestroy {
   private isFacultyDialogOpen = false;
   private isRedirectDialogOpen = false;
 
-  readonly backgroundImages = [
+  readonly slideshowImages = [
     'assets/images/pupt_img_1.webp',
     'assets/images/pupt_img_2.webp',
     'assets/images/pupt_img_3.webp',
@@ -48,58 +48,28 @@ export class LoginComponent implements OnInit, OnDestroy {
     'assets/images/pupt_img_5.webp',
   ];
 
-  readonly slideshowImages = this.backgroundImages.map(
-    (img) => `assets/images/${img.split('/').pop()}`
-  );
-
-  currentBackgroundImage: string;
-
+  currentBackgroundImage = '';
   isDarkTheme$: Observable<boolean>;
 
   constructor(
-    private renderer: Renderer2,
-    private elementRef: ElementRef,
     private themeService: ThemeService,
     private dialog: MatDialog,
     private authService: AuthService
   ) {
     this.isDarkTheme$ = this.themeService.isDarkTheme$;
-    this.currentBackgroundImage = `url(${this.backgroundImages[0]})`;
   }
 
-  ngOnInit(): void {
-    this.updateBackgroundImage(0);
-    this.isDarkTheme$.pipe(takeUntil(this.destroy$)).subscribe();
+  ngOnInit() {
+    this.currentBackgroundImage = `url(${this.slideshowImages[0]})`;
   }
 
-  ngOnDestroy(): void {
+  ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
   }
 
-  get isDarkTheme(): Observable<boolean> {
-    return this.themeService.isDarkTheme$;
-  }
-
-  toggleTheme(): void {
-    this.themeService.toggleTheme();
-  }
-
-  onSlideChange(index: number): void {
-    this.updateBackgroundImage(index);
-  }
-
-  updateBackgroundImage(index: number): void {
-    this.currentBackgroundImage = `url(${this.backgroundImages[index]})`;
-    const loginContainer =
-      this.elementRef.nativeElement.querySelector('.login-container');
-    if (loginContainer) {
-      this.renderer.setStyle(
-        loginContainer,
-        'background-image',
-        this.currentBackgroundImage
-      );
-    }
+  onSlideChange(index: number) {
+    this.currentBackgroundImage = `url(${this.slideshowImages[index]})`;
   }
 
   handleFacultyLogin(): void {
@@ -115,8 +85,8 @@ export class LoginComponent implements OnInit, OnDestroy {
       this.isRedirectDialogOpen = false;
     });
 
-    forkJoin([this.authService.checkHrisHealth(), timer(2000)]).subscribe({
-      next: ([isHealthy]) => {
+    this.authService.checkHrisHealth().subscribe({
+      next: (isHealthy) => {
         if (isHealthy) {
           dialogRef.componentInstance.updateState(false, true);
         } else {
@@ -156,5 +126,9 @@ export class LoginComponent implements OnInit, OnDestroy {
     dialogRef.afterClosed().subscribe(() => {
       this.isAdminDialogOpen = false;
     });
+  }
+
+  toggleTheme(): void {
+    this.themeService.toggleTheme();
   }
 }
